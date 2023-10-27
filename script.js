7e8d712ef706fd6469195fb1239a70653427a68f
LunevaDariaa <daria.safri@gmail.com>
2023-10-26 23:25:30 -0400

    Created function,that create 5 rounds  and calculate who win the game

--- script.js
+++ script.js
@@ -18,17 +18,10 @@ const playerSelection = function () {
     );
   }
 };
-//playerChoise();
-
-// compute random choise of computer
-const getComputerChoise = function () {
-  return choise[Math.floor(Math.random() * choise.length)];
-};
-//getComputerChoise();
 
 // compare two inputs
 
-const computerChoise = getComputerChoise();
+//const computerChoise = getComputerChoise();
 const playerChoise = playerSelection();
 const playRound = function (playerChoise, computerChoise) {
   const lose = `You lose! ${computerChoise} beats ${playerChoise}!`;
@@ -41,25 +34,53 @@ const playRound = function (playerChoise, computerChoise) {
     playerChoise === "scissors"
   ) {
     if (playerChoise === "paper" && computerChoise === "rock") {
+      playerScore++;
       return won;
     } else if (playerChoise === "paper" && computerChoise === "scissors") {
+      computerScore++;
       return lose;
     } else if (playerChoise === "scissors" && computerChoise === "paper") {
+      playerScore++;
       return won;
     } else if (playerChoise === "scissors" && computerChoise === "rock") {
+      computerScore++;
       return lose;
     } else if (playerChoise === "rock" && computerChoise === "scissors") {
+      playerScore++;
       return won;
     } else if (playerChoise === "rock" && computerChoise === "paper") {
+      computerScore++;
       return lose;
     } else if (playerChoise === computerChoise) {
       return `Try again! You both choose ${playerChoise}`;
     }
   }
 };
-console.log(playRound(playerChoise, computerChoise));
+let playerScore = 0;
+let computerScore = 0;
 // if there is rock and scissor => rock wins
 // if rock and paper => paper wins
 // if paper and scissors => scissor wins
 // decide who win this round- user or computer
 // plus one point to one, who won
+const game = function () {
+  for (let i = 0; i < 5; i++) {
+    playerSelection(); // Get user's choice
+    const computerChoise = choise[Math.floor(Math.random() * choise.length)]; // Get a random computer choice
+    console.log(playRound(playerChoise, computerChoise));
+    if (playerChoise === computerChoise) {
+      i--;
+    }
+  }
+  if (computerScore > playerScore) {
+    console.log("Computer won that game");
+    console.log(playerScore);
+    console.log(computerScore);
+  } else {
+    console.log("Congratulations! You won that game!");
+    console.log(playerScore);
+    console.log(computerScore);
+  }
+};
+
+game();
