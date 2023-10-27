a19a69aea6d662005a232e7eb5fd3d2f1e34c043
LunevaDariaa <daria.safri@gmail.com>
2023-10-26 22:30:10 -0400

    Created functions with actual rules of a game: when wins user and when computer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // create rock, scissor and paper
-const choise = ["rock", "paper", "scissor"];
+const choise = ["rock", "paper", "scissors"];
 // take user's input- one of them
 const playerSelection = function () {
   const input = prompt("Choose among Paper, Scissors and Rock");
@@ -11,20 +11,53 @@ const playerSelection = function () {
     inputLowerCase === "scissors" ||
     inputLowerCase === "paper"
   ) {
-    console.log(inputLowerCase);
+    return inputLowerCase;
   } else {
     console.log(
       "Please, correct your answer. Only Paper, Scissors or Rock can play"
     );
   }
 };
-playerSelection();
+//playerChoise();
+
 // compute random choise of computer
-const getComputedChoise = function () {
-  console.log(choise[Math.floor(Math.random() * choise.length)]);
+const getComputerChoise = function () {
+  return choise[Math.floor(Math.random() * choise.length)];
 };
-getComputedChoise();
+//getComputerChoise();
+
 // compare two inputs
+
+const computerChoise = getComputerChoise();
+const playerChoise = playerSelection();
+const playRound = function (playerChoise, computerChoise) {
+  const lose = `You lose! ${computerChoise} beats ${playerChoise}!`;
+  const won = `You won! ${playerChoise} beats ${computerChoise}!`;
+  //console.log(playerChoise);
+  //console.log(computerChoise);
+  while (
+    playerChoise === "paper" ||
+    playerChoise === "rock" ||
+    playerChoise === "scissors"
+  ) {
+    if (playerChoise === "paper" && computerChoise === "rock") {
+      return won;
+    } else if (playerChoise === "paper" && computerChoise === "scissors") {
+      return lose;
+    } else if (playerChoise === "scissors" && computerChoise === "paper") {
+      return won;
+    } else if (playerChoise === "scissors" && computerChoise === "rock") {
+      return lose;
+    } else if (playerChoise === "rock" && computerChoise === "scissors") {
+      return won;
+    } else if (playerChoise === "rock" && computerChoise === "paper") {
+      return lose;
+    } else if (playerChoise === computerChoise) {
+      return `Try again! You both choose ${playerChoise}`;
+    }
+  }
+};
+console.log(playRound(playerChoise, computerChoise));
 // if there is rock and scissor => rock wins
 // if rock and paper => paper wins
 // if paper and scissors => scissor wins
