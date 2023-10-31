b8a1d4cf5557038f392eb3fd5c0c365aa5b1d9f7
LunevaDariaa <daria.safri@gmail.com>
2023-10-31 18:49:04 -0400

    Added functionality, created the modal

--- script.js
+++ script.js
@@ -12,8 +12,13 @@ const compScr = document.querySelector(".comp_score");
 const infoDisplay = document.querySelector(".info_display");
 const choise = ["rock", "paper", "scissors"];
 
+const popUp = document.querySelector(".popup");
+const overLay = document.querySelector(".overlay");
+const winner = document.querySelector(".winner");
+const againBtn = document.querySelector(".btn_close");
 let playerScore = 0;
 let computerScore = 0;
+
 // Compute computer input
 let computerChoise;
 const compSelection = function (array) {
@@ -23,6 +28,33 @@ const compSelection = function (array) {
   return computerChoise;
 };
 
+//Show popup
+const showModal = function () {
+  if (playerScore >= 5) {
+    popUp.classList.remove("hide");
+    overLay.classList.remove("hide");
+    winner.textContent = "You won this game!";
+  } else if (computerScore >= 5) {
+    popUp.classList.remove("hide");
+    overLay.classList.remove("hide");
+    winner.textContent = "Gosh, you lost :(";
+  }
+};
+
+const playAgain = function () {
+  againBtn.addEventListener("click", function () {
+    popUp.classList.add("hide");
+    overLay.classList.add("hide");
+    playerScore = 0;
+    computerScore = 0;
+    imgUser.classList.add("hide");
+    imgComp.classList.add("hide");
+    userScr.innerHTML = 0;
+    compScr.innerHTML = 0;
+    infoDisplay.innerHTML = "";
+  });
+};
+//Running game
 btn.forEach((button) => {
   button.addEventListener("click", function () {
     if (button == rock) {
@@ -39,9 +71,12 @@ btn.forEach((button) => {
     playRound(button.id, computerChoise);
     userScr.textContent = playerScore;
     compScr.textContent = computerScore;
+    showModal();
+    playAgain();
   });
 });
 
+//Game rules plus score couter
 const playRound = function (buttonId, computerChoise) {
   const lose = `You lose! ${computerChoise} beats ${buttonId}!`;
   const won = `You won! ${buttonId} beats ${computerChoise}!`;
@@ -52,110 +87,20 @@ const playRound = function (buttonId, computerChoise) {
     infoDisplay.textContent = won;
   } else if (buttonId === "paper" && computerChoise === "scissors") {
     computerScore++;
-    infoDisplay.textContent = won;
+    infoDisplay.textContent = lose;
   } else if (buttonId === "scissors" && computerChoise === "paper") {
     playerScore++;
-    return won;
+    infoDisplay.textContent = won;
   } else if (buttonId === "scissors" && computerChoise === "rock") {
     computerScore++;
-    infoDisplay.textContent = won;
+    infoDisplay.textContent = lose;
   } else if (buttonId === "rock" && computerChoise === "scissors") {
     playerScore++;
-    return won;
+    infoDisplay.textContent = won;
   } else if (buttonId === "rock" && computerChoise === "paper") {
     computerScore++;
-    infoDisplay.textContent = won;
+    infoDisplay.textContent = lose;
   } else if (buttonId === computerChoise) {
     infoDisplay.textContent = `Try again! You both choose ${buttonId}`;
   }
 };
-
-// console.log(playerScore);
-// console.log(computerScore);
-/*
-// create rock, scissor and paper
-const choise = ["rock", "paper", "scissors"];
-// take user's input- one of them
-const playerSelection = function () {
-  const input = prompt("Choose among Paper, Scissors and Rock");
-  const inputLowerCase = input.toLowerCase();
-  if (
-    inputLowerCase === "rock" ||
-    inputLowerCase === "scissors" ||
-    inputLowerCase === "paper"
-  ) {
-    return inputLowerCase;
-  } else {
-    console.log(
-      "Please, correct your answer. Only Paper, Scissors or Rock can play"
-    );
-  }
-};
-
-// Compute computer input
-let computerChoise;
-const compSelection = function (array) {
-  computerChoise = choise[Math.floor(Math.random() * choise.length)]; // Get a random computer choice
-  return computerChoise;
-};
-
-// compare two inputs
-const playRound = function (playerChoise, computerChoise) {
-  const lose = `You lose! ${computerChoise} beats ${playerChoise}!`;
-  const won = `You won! ${playerChoise} beats ${computerChoise}!`;
-  //whet input right - game will start
-  while (
-    playerChoise === "paper" ||
-    playerChoise === "rock" ||
-    playerChoise === "scissors"
-  ) {
-    //The rules of the game
-    if (playerChoise === "paper" && computerChoise === "rock") {
-      playerScore++;
-      return won;
-    } else if (playerChoise === "paper" && computerChoise === "scissors") {
-      computerScore++;
-      return lose;
-    } else if (playerChoise === "scissors" && computerChoise === "paper") {
-      playerScore++;
-      return won;
-    } else if (playerChoise === "scissors" && computerChoise === "rock") {
-      computerScore++;
-      return lose;
-    } else if (playerChoise === "rock" && computerChoise === "scissors") {
-      playerScore++;
-      return won;
-    } else if (playerChoise === "rock" && computerChoise === "paper") {
-      computerScore++;
-      return lose;
-    } else if (playerChoise === computerChoise) {
-      return `Try again! You both choose ${playerChoise}`;
-    }
-  }
-};
-let playerScore = 0;
-let computerScore = 0;
-
-const game = function () {
-  for (let i = 0; i < 5; i++) {
-    const playerChoise = playerSelection(); // Get user's choice
-    compSelection(choise);
-    console.log(playRound(playerChoise, computerChoise));
-    if (playerChoise === computerChoise) {
-      i--;
-    }
-  }
-  // Calculate final points and display who win the game
-  if (computerScore > playerScore) {
-    console.log("Computer won that game");
-    console.log(`Your score: ${playerScore}`);
-    console.log(`Your score: ${computerScore}`);
-  } else {
-    console.log("Congratulations! You won that game!");
-    console.log(`Your score: ${playerScore}`);
-    console.log(`Your score: ${computerScore}`);
-  }
-};
-
-game();
-*/
